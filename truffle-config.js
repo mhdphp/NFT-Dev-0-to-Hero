e44f21fa3aabe1128e5b99f43df2e6d0e0ddf46d
Mihai Enuica <mihai.enuica@gmail.com>
2022-02-20 20:28:42 +0200

    Before first compile the contract

--- truffle-config.js
+++ truffle-config.js
@@ -7,14 +7,14 @@ module.exports = {
        port: 7545,            // Standard Ethereum port (default: none)
        network_id: "*",       // Any network (default: none)
       },
-
-      // since we have change the place of contracts from the root into src/contracts
-      contracts_directory: "./scr/contracts",
-
-      // The json interface is a json object describing the Application Binary Interface (ABI) for an Ethereum smart contract.
-      // the folder does't exist but will be created
-      contracts_build_directory: "./src/abis",
     },
+
+     // since we have change the place of contracts from the root into src/contracts
+     contracts_directory: "./scr/contracts",
+
+     // The json interface is a json object describing the Application Binary Interface (ABI) for an Ethereum smart contract.
+     // the folder does't exist but will be created
+     contracts_build_directory: "./src/abis",
   
     // Configure your compilers
     compilers: {
