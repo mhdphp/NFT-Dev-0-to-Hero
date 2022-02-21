4d9dd1e41c0800f231eb0ac90c655ef373351d0b
Mihai Enuica <mihai.enuica@gmail.com>
2022-02-21 21:08:39 +0200

    Start creation of the minting contract

---
+++ src/contracts/ERC721.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+/*
+Building up the minting function
+    a. NFT point to an address,
+    b. Keep track of the token ids,
+    c. Keep track of token owner address to token id,
+    d. Keep track of how many tokens an owner has,
+    e. Create an event that emit a transfer log:
+        - contract address,
+        - token id,
+        - owener,
+        - where is being minted to
+        - etc
+*/
+
+contract ERC721 {
+
+}
