51b5204d6cb3b2c01555bd1de6dff33a6f4917a5
Mihai Enuica <mihai.enuica@gmail.com>
2022-02-22 12:06:04 +0200

    The ERC721.sol and the _mint function

--- src/contracts/ERC721.sol
+++ src/contracts/ERC721.sol
@@ -17,4 +17,40 @@ Building up the minting function
 
 contract ERC721 {
 
+    // address from - the contract address
+    // address to - the owner address
+    event Transfer(
+        address indexed from, 
+        address indexed to, 
+        uint256 indexed tokenId);
+
+    // mapping in solidity creates a hash table of key pair values
+    // mapping from token id to the owner
+    mapping(uint256 => address) private _tokenOwner;
+
+    // mappint from owner to number of owned tokens
+    mapping(address => uint256) private _OwnedTokensCount;
+
+    function _exists(uint256 tokenId) internal view returns(bool){
+        // setting the address of the nft owner to a variable
+        address owner = _tokenOwner[tokenId];
+        // return truthiness that address is not zero;
+        // return truth if the address is not zero, so the token exists
+        return owner != address(0);
+    }
+
+    function _mint(address to, uint256 tokenId) internal {
+        // require that the address is not zero (it is a valid address)
+        require(to != address(0), 'ERC721: minting to a zero address');
+        // require that the tokenId doesn't exist (was not minted previously)
+        require(!_exists(tokenId), 'ERC721: token already minted (exists)');
+        // we are adding a new address to the tokenId for minting
+        _tokenOwner[tokenId] = to;
+        // we increase the counter of Owner address with one unit (total minted tokens)
+        _OwnedTokensCount[to] += 1;
+
+        // for the moment we keept the contract address to zero
+        emit Transfer(address(0), to, tokenId);
+    }
+
 }
