57f1291d54bcd1db9093d0eaa8c0224ddbf8ceb3
Mihai Enuica <mihai.enuica@gmail.com>
2022-03-07 15:47:51 +0200

    Truffle first test

---
+++ test/KryptoBird.test.js
@@ -0,0 +1,30 @@
+
+// import assert lib from chaijs
+const assert = require('assert');
+
+// Kryptobird the name from 2_deploy_contracts.js
+const Kbird = artifacts.require('Kryptobird');
+
+// check for chai
+require('chai')
+.use(require('chai-as-promised'))
+.should()
+
+contract('Kbird', (accounts) =>{
+    let contract;
+
+    // testing container - describe
+    describe('deployment', async()=>{
+        
+        // test samples with writing it
+        it('deploys successfully', async()=>{
+            contract = await Kbird.deployed();
+            const address = contract.address;
+            assert.notEqual(address, '');
+            assert.notEqual(address, null);
+            assert.notEqual(address, undefined);
+            assert.notEqual(address, 0x0);
+        });
+    });
+
+});
