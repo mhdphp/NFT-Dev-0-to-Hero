505e4e2b5f05113835335a969ed28a84b0de8626
Mihai Enuica <mihai.enuica@gmail.com>
2022-03-08 17:26:57 +0200

    All tests passed except minting the same str token

--- test/KryptoBird.test.js
+++ test/KryptoBird.test.js
@@ -10,16 +10,22 @@ require('chai')
 .use(require('chai-as-promised'))
 .should();
 
+// accounts is an array of all the accounts in ganache app
+// accounts[0] is the first account - msg.sender
 contract('Kbird', (accounts) =>{
     let contract;
+    // before tells our tests to run this first before anything eles
+    before( async() =>{
+        contract = await Kbird.deployed();
+    });
+    
 
     // testing container - describe
     describe('deployment', async()=>{
         
         // test samples with writing it
         it('deploys successfully', async()=>{
-            contract = await Kbird.deployed();
-            const address = contract.address;
+            const address = await contract.address;
             assert.notEqual(address, '');
             assert.notEqual(address, null);
             assert.notEqual(address, undefined);
@@ -37,4 +43,29 @@ contract('Kbird', (accounts) =>{
         });
     });
 
+    describe('minting', async()=>{
+
+        it('creates a new token', async() =>{
+            const result = await contract.mint('http...1');
+            const totalSupply = await contract.totalSupply();
+            // get the event from function mint
+            const event = result.logs[0].args;
+
+            console.log('Total Supply = ', totalSupply.toString())
+            console.log('Total Supply (words[0]) = ', totalSupply.words[0])
+            console.log('Total Supply (.length) = ', totalSupply.length)
+            
+            //success
+            assert.equal(totalSupply, 1);
+            assert.equal(event._from, 
+                '0x0000000000000000000000000000000000000000', 'from address the contract');
+            assert.equal(event._to, accounts[0], 'to is msg.sender');
+
+            console.log('_to=', event._to);
+            
+            //failure
+            // don't work
+            //await contract.mint('http...1').should.be.rejected;
+        });
+    });
 });
